1f97182ce848a22e0f9494e62b7e7bdacf930475
tankorsmash <tankorsmash@gmail.com>
2015-03-01 08:03:33 -0500

    click on aisle to show it in list

--- static/js/grocery.js
+++ static/js/grocery.js
@@ -1,2 +1,19 @@
 $(function() {
+    $("#aisle-map a").click(function(e) {
+        that = $(this);
+        attr_id = that.attr("id");
+        aisle_elem = $("#aisle-"+attr_id); 
+        if (attr_id) 
+        {
+            if (aisle_elem.hasClass("active-aisle"))
+            {
+                aisle_elem.removeClass("active-aisle");
+            }
+            else
+            {
+                aisle_elem.addClass("active-aisle");
+
+            };
+        };
+    });
 });
