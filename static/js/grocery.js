1974fafdbefccaa536a599875417e3da47f8ec49
tankorsmash <tankorsmash@gmail.com>
2015-03-01 08:08:10 -0500

    add hover effect on list

--- static/js/grocery.js
+++ static/js/grocery.js
@@ -12,7 +12,24 @@ $(function() {
             else
             {
                 aisle_elem.addClass("active-aisle");
+            };
+        };
+    });
 
+    $("[id^=aisle-]").hover(function(e) {
+        that = $(this);
+        attr_id = that.attr("id");
+        if (attr_id) 
+        {
+            attr_id = that.attr("id").replace("aisle-", "");
+            map_elem = $("#"+attr_id); 
+            if (map_elem.hasClass("active-aisle"))
+            {
+                map_elem.removeClass("active-aisle");
+            }
+            else
+            {
+                map_elem.addClass("active-aisle");
             };
         };
     });
